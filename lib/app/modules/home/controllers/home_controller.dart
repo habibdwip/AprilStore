b449faa099219321b258dfa6a40983cbe7cb1df8
Habib Dwi Prajoto <habibdwi73@gmail.com>
2024-04-28 16:40:27 +0700

    [UPDATE] home page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,21 +1,55 @@
+import 'package:april_store/app/model/produk_model.dart';
+import 'package:april_store/app/services/all_services.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  final count = 0.obs;
+  final AllServices allServices = AllServices();
+  final int limit = 10;
+  int skip = 0;
+  var hasMore = true.obs;
+  var produk = <Products>[].obs;
+  TextEditingController SearchController = TextEditingController();
+  var cari = <Products>[].obs;
+
   @override
   void onInit() {
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future searchProduk() async {
+    try {
+      List<Products> response =
+          await allServices.searchProduk(SearchController.text);
+      cari.value = [];
+      cari.addAll(response);
+      update();
+    } catch (e) {
+      if (kDebugMode) print(e.toString());
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  Future getProduk() async {
+    try {
+      List<Products> response = await allServices.getProduk(limit, skip);
+      if (response.length < limit) {
+        hasMore.value = false;
+      }
+
+      produk.addAll(response);
+      skip += 10;
+      update();
+    } catch (e) {
+      if (kDebugMode) print(e.toString());
+    }
   }
 
-  void increment() => count.value++;
+  Future refreshData() async {
+    skip = 0;
+    hasMore.value = true;
+    produk.value = [];
+
+    await getProduk();
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:april_store/app/modules/home/views/detail_page.dart';
+import 'package:april_store/app/modules/home/views/search_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -8,16 +10,74 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final ScrollController scrollController = ScrollController();
+    final HomeController state = Get.put(HomeController());
+    state.getProduk();
+
+    Future onRefresh() async {
+      state.refreshData();
+    }
+
+    void onScroll() {
+      double maxScroll = scrollController.position.maxScrollExtent;
+      double currentScroll = scrollController.position.pixels;
+
+      if (maxScroll == currentScroll && state.hasMore.value) {
+        state.getProduk();
+      }
+    }
+
+    scrollController.addListener(onScroll);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        title: const Text(
+          'April Store',
+          style: TextStyle(color: Colors.white),
         ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+              onPressed: () => Get.to(() => SearchPage()),
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: Obx(() => ListView.builder(
+            controller: scrollController,
+            itemCount: state.hasMore.value
+                ? state.produk.length + 1
+                : state.produk.length,
+            itemBuilder: (context, index) {
+              if (index < state.produk.length) {
+                return ListTile(
+                  onTap: () {
+                    Get.to(() => DetailPage(
+                          products: state.produk[index],
+                        ));
+                  },
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(state.produk[index].thumbnail ?? ''),
+                  ),
+                  title: Text(state.produk[index].title ?? ''),
+                  subtitle: Text(state.produk[index].brand ?? ''),
+                );
+              } else {
+                return const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.black,
+                    ),
+                  ),
+                );
+              }
+            })),
       ),
     );
   }
